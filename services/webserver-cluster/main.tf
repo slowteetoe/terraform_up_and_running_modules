c9d7ad1c64893ce8ad0c89463a2daad2bd8af98b
Steven Lotito <steven.lotito@gmail.com>
2020-02-01 10:38:55 -0800

    add some alarms

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -174,3 +174,40 @@ resource "aws_lb_target_group" "asg" {
     unhealthy_threshold = 2
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "high_cpu_utilization" {
+  alarm_name  = "${var.cluster_name}-high-cpu-utilization"
+  namespace   = "AWS/EC2"
+  metric_name = "CPUUtilization"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.example.name
+  }
+
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  period              = 300
+  statistic           = "Average"
+  threshold           = 90
+  unit                = "Percent"
+}
+
+resource "aws_cloudwatch_metric_alarm" "low_cpu_credit_balance" {
+  # only t2.xxxx resources have a CPU credit, so we need this to be conditional
+  count = format("%.2s", var.instance_type) == "t2" ? 1 : 0
+
+  alarm_name  = "${var.cluster_name}-low-cpu-credit-balance"
+  namespace   = "AWS/EC2"
+  metric_name = "CPUCreditBalance"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.example.name
+  }
+
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 1
+  period              = 300
+  statistic           = "Minimum"
+  threshold           = 10
+  unit                = "Count"
+}
